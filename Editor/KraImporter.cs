eb09efe9ea86b30f0385fb51839fcb32be972317
HaroldLever <Harold_Lever@outlook.com>
2024-12-05 22:14:54 +0800

    Implement importing kra file as sprite

--- Editor/KraImporter.cs
+++ Editor/KraImporter.cs
@@ -1,4 +1,7 @@
+using System;
+using System.IO;
 using System.IO.Compression;
+using UnityEditor;
 using UnityEngine;
 using UnityEditor.AssetImporters;
 
@@ -7,7 +10,14 @@ namespace HNL
     [ScriptedImporter(1, "kra")]
     public class KraImporter : ScriptedImporter
     {
-        // public TextureFormat textureFormat;
+        public enum TextureType
+        {
+            Texture2D,
+            Sprite,
+        }
+        
+        public TextureType textureType = TextureType.Texture2D;
+        public SpriteSettings spriteSettings = new SpriteSettings();
         public TextureWrapMode wrapMode = TextureWrapMode.Repeat;
         public FilterMode filterMode = FilterMode.Bilinear;
         public int anisoLevel = 1;
@@ -23,7 +33,7 @@ namespace HNL
 
                 if (mergedImageEntry == null)
                 {
-                    Debug.LogWarning($"{path} 不含 mergedimage.png");
+                    Debug.LogWarning($"{path} 不含 mergedimage.png\n{path} has no mergedimage.png");
                 }
                 else
                 {
@@ -41,11 +51,71 @@ namespace HNL
                         texture.anisoLevel = anisoLevel;
                         // texture.Apply();
 
-                        ctx.AddObjectToAsset("merged image", texture);
+                        ctx.AddObjectToAsset("merged image", texture, texture);
                         ctx.SetMainObject(texture);
+
+                        if (textureType == TextureType.Sprite)
+                        {
+                            var sprite = Sprite.Create(
+                                texture,
+                                spriteSettings.GetRect(texture),
+                                spriteSettings.pivot,
+                                spriteSettings.pixelsPerUnit,
+                                spriteSettings.extrude,
+                                spriteSettings.meshType,
+                                spriteSettings.GetBorder(texture)
+                                );
+                            sprite.name = Path.GetFileNameWithoutExtension(path);
+                            sprite.hideFlags = HideFlags.None;
+                            ctx.AddObjectToAsset("sprite", sprite);
+                        }
                     }
                 }
             }
+        }
+        
+        [Serializable]
+        public class SpriteSettings
+        {
+            public Rect normalizedRect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
+            public Vector2 pivot = new Vector2(0.5f, 0.5f);
+            public float pixelsPerUnit = 100.0f;
+            public uint extrude = 0;
+            public SpriteMeshType meshType = SpriteMeshType.Tight;
+            public Vector4 normalizedBorder = Vector4.zero;
+            public bool generateFallbackPhysicsShape = false;
+
+
+            public Rect GetRect(int width, int height)
+            {
+                return new Rect(
+                    normalizedRect.x * width,
+                    normalizedRect.y * height,
+                    normalizedRect.width * width,
+                    normalizedRect.height * height
+                );
+            }
+            
+            public Rect GetRect(Texture2D texture)
+            {
+                return GetRect(texture.width, texture.height);
+            }
+
+            public Vector4 GetBorder(int width, int height)
+            {
+                return new Vector4(
+                    normalizedBorder.x * width,
+                    normalizedBorder.y * height,
+                    normalizedBorder.z * width,
+                    normalizedBorder.w * height
+                );
+            }
+
+            public Vector4 GetBorder(Texture2D texture)
+            {
+                return GetBorder(texture.width, texture.height);
+            }
+            
         }
     }
 }
--- Editor/KraImporterEditor.cs
+++ Editor/KraImporterEditor.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using System.Reflection;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.UIElements;
@@ -21,7 +24,12 @@ namespace HNL
                 if (currentProperty.propertyPath == "m_Script")
                     propField.SetEnabled(false);
                 root.Add(propField);
-            } while (currentProperty.NextVisible(true));
+            } while (currentProperty.NextVisible(currentProperty.name == "Base"));
+            
+            var type = Type.GetType("UnityEditor.U2D.Sprites.SpriteEditorWindow,Unity.2D.Sprite.Editor");
+            var showSpriteEditor = new Button() { text = "Show Sprite Editor" };
+            showSpriteEditor.clicked += () => { EditorWindow.GetWindow(type).Show(); };
+            root.Add(showSpriteEditor);
 
             var imgui = new IMGUIContainer();
             imgui.onGUIHandler += IMGUI;
@@ -29,13 +37,6 @@ namespace HNL
 
             return root;
         }
-
-        // public override void OnInspectorGUI()
-        // {
-        //     serializedObject.Update();
-        //     serializedObject.ApplyModifiedProperties();
-        //     base.ApplyRevertGUI();
-        // }
 
         private void IMGUI()
         {
